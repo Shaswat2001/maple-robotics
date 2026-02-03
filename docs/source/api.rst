ef52df77a7d9860cde1240dbdc07716a52d89332
Shaswat2001 <sis_shaswat@outlook.com>
2026-02-03 02:33:42 -0500

    add templates

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -1,10 +1,6 @@
 API
 ===
-.. automodule:: maple
-   :members:
-   :undoc-members:
-   :show-inheritance:
-   
+
 .. autosummary::
    :recursive:
    :toctree: generated
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ author = "Shaswat Garg"
 
 # The full version, including alpha/beta/rc tags
 version = release = "0.0.2"
-autosummary_generate = True
 
 # -- General configuration ---------------------------------------------------
 
